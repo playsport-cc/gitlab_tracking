ce6fa986af2d9f0a6f624ced168c5a60b1e8ba90
pauleanr <pauleanr.fe@gmail.com>
2018-12-07 15:39:37 +0800

    [BUG] fix migration error

--- db/migrate/003_add_columns_to_gitlab_tracking_commits.rb
+++ db/migrate/003_add_columns_to_gitlab_tracking_commits.rb
@@ -1,10 +1,6 @@
 class AddColumnsToGitlabTrackingCommits < ActiveRecord::Migration
-  def self.up
+  def change
     add_column :gitlab_tracking_commits, :merge_request_id, :integer
     add_column :gitlab_tracking_commits, :merge_request_url, :string, :limit => 4096
-  end
-
-  def self.down
-    remove_column :gitlab_tracking_commits, :merge_request_id
   end
 end
